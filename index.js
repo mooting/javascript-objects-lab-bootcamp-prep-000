2f5a4c844dcc71ae1c1bd6e314f98eda81fad24d
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 19:53:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue( object, key, value ) {
+  return Object.assign({ [key]: value }, object )
+}
+
+function destructivelyUpdateObjectWithKeyAndValue( object, key, value ) {
+  return Object.assign( object, { [key]: value })
+}
+
+function deleteFromObjectByKey( object, key ) {
+  var returnObject = Object.assign( {}, object )
+  console.log( `returnObject: ${ JSON.stringify( returnObject )}` )
+  console.log( `key: ${ key }`)
+  delete returnObject.key
+  console.log( `returnObjectAfter: ${ JSON.stringify( returnObject )}` )
+  return returnObject
+}
